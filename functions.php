a7d47df9353cbf953c1436d1c1c0dabc0f6673e3
Naviddev1 <Navid86@hotmail.se>
2023-04-23 19:48:24 +0200

    Template overrite done on archive

--- functions.php
+++ functions.php
@@ -7,3 +7,9 @@ if (! defined('WP_DEBUG')) {
 add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 });
+
+// Gör en template overrite på archive-product filen, hälsar kunden välkommen.
+function mytheme_add_woocommerce_support() {
+	add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
