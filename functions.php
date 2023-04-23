fe41fed5a913b8731c162c70d8e129c4b1a3ce9e
Naviddev1 <Navid86@hotmail.se>
2023-04-23 20:55:24 +0200

    removing the admin bar by using filter

--- functions.php
+++ functions.php
@@ -13,3 +13,20 @@ function mytheme_add_woocommerce_support() {
 	add_theme_support( 'woocommerce' );
 }
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+
+
+// changing the letter k to m
+
+function replace_k_with_m($var) {
+
+	$letter = str_replace('k', 'm', $var);
+	
+	return $letter;
+	
+	}
+	
+	add_action('the_title', 'replace_k_with_m');
+
+
+	// using the filter moce to remove someting, in this case the admin bar for logged in users.
+	add_filter('show_admin_bar', '__return_false');
